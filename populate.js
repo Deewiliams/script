dbd259ef112eb9be00b476838a475891e614fa54
Desire <desireirankundawilliams@gmail.com>
2022-07-13 14:26:13 +0200

    cleaned the code

--- populate.js
+++ populate.js
@@ -6,13 +6,12 @@ dotenv.config();
 const productScript = require('./models/product')
 const productJson = require('./products.json')
 
-
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-         await productScript.deleteMany();
+        await productScript.deleteMany();
         await productScript.create(productJson)
         app.listen(port, console.log(`server is listening on port ${port}`));
     } catch (error) {
--- routes/products.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const {createProduct} = require('../controllers/products')
-
-router.route.post(createProduct)
-
-
-module.exports = router
